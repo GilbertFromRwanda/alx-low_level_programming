e98a9e644072ff5452fbd0c29a07ba020f765472
GilbertFromRwanda <askforgilbert@gmail.com>
2023-02-22 02:08:01 -0800

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,10 +3,8 @@
 #include "main.h"
 /**
  * _islower - check the code for checking if character is lower
+ *@c: c take any integer value
  *
- * Description:'The program check if given c islower'
- *
- *@parameterc: c take any integer value
  * Return: Always 0.
  */
 
