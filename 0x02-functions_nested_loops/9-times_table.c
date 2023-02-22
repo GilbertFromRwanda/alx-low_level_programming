59110474ed51874f3d4d804aaacc406266b55c00
GilbertFromRwanda <askforgilbert@gmail.com>
2023-02-22 04:13:22 -0800

    learn time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,14 +7,14 @@
  */
 void times_table(void)
 {
-  int i = 0, j = 0, p = 1,nl=1;
+  int i = 0, j = 0, p = 1, nl = 1;
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
 p = i * j;
 nl = i * (j + 1);
-if (nl < 10 && j !=9)
+if (nl < 10 && j != 9)
 {
 printf("%d,  ", p);
 }
@@ -24,11 +24,11 @@ printf("%d, ", p);
 }
 else if (j == 9 && nl > 10)
 {
-printf("%d  ", p);
+printf("%d", p);
 }
 else
 {
-printf("%d ", p);
+printf("%d", p);
 }
 }
 printf("\n");
