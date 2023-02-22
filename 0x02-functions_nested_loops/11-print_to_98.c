06cd26a557338d518b29f74df716c06d8dc2edca
GilbertFromRwanda <askforgilbert@gmail.com>
2023-02-22 04:52:51 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,23 +9,24 @@ void print_to_98(int n)
 {
 if (n == 98)
 {
-printf("%d ", n);
-_putchar('\n');
+printf("%d \n", n);
 }
 else if (n > 98)
 {
-while (n >= 98)
+while (n > 98)
 {
 printf("%d, ", n);
 --n;
 }
+printf("98");
 }
 else 
 {
-while (n <= 98)
+while (n < 98)
 {
 printf("%d, ", n);
 n++;
 }
+printf("98");
 }
 }
