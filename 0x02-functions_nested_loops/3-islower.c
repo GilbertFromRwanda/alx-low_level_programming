6a42e9d70951f8d0276a79b95a290168b850197d
GilbertFromRwanda <askforgilbert@gmail.com>
2023-02-22 01:45:43 -0800

    fix islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,23 +2,11 @@
 #include<ctype.h>
 #include "main.h"
 /**
- * main - check the code
+ * _islower - check the code for checking if character is lower
  *
  * Return: Always 0.
  */
-int _islower(int c);
-int main(void)
-{
-int r;
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
-return (0);
-}
+
 int _islower(int c)
 {
   return islower(c) > 0 ? 1 : 0;
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,7 +5,13 @@ extern void _putchar(char c);
 int  putchar(int  c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-
+int _islower(int c);
+int print_sign(int n);
+int _isalpha(int c);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
 /* _putchar
 void _putchar(char c)
 {
