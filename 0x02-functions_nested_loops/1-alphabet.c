b87e0ba1cf481404c5668caa9aac533a4ed9eec1
GilbertFromRwanda <askforgilbert@gmail.com>
2023-02-22 01:29:43 -0800

    add print alphabert in main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,11 @@
 #include<stdio.h>
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - display alphabet
  *
  * Return: Always 0.
  */
 
-/*void print_alphabet(void);
-
-int main(void)
-{
-print_alphabet();
-return (0);
-}
-*/
 void print_alphabet(void)
 {
 char c;
