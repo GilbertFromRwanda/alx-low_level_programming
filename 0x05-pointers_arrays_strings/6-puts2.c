75ff21378936235a178f3f226dcc683266e5654b
GilbertFromRwanda <askforgilbert@gmail.com>
2023-02-27 08:39:17 -0800

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* puts - function code
+*@str: string
+* Return: 0
+*/
+void puts2(char *str)
+{
+int g, i = strlen(str);
+for (g = 0; g < i; g = g + 2)
+{
+_putchar(str[g]);
+}
+_putchar('\n');
+}
