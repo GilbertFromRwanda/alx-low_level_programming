ccf0441c8abd54ba4f90dcc75d28d7846b5ef9d0
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-02 01:56:16 -0800

    add two large integers

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* infinite_add - function code
+*@n1: integer value
+*@n2: integer
+*@r: buffe size
+*@size_r: size
+* Return: 0
+*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+return (atoi(n1) + atoi(n2));
+}
