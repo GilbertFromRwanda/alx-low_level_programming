111ef69b4d3fe38624b61349b365d5a0126d974c
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-02 00:49:45 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+* leet - function code
+*@str: string
+*
+* 
+*Return: string
+*/
+char *leet(char *str)
+{
+int i = 0;
+char *sub = "aAeEoOtTlL";
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == 'a' || str[i] == 'A')
+{
+str[i] = '4';
+}
+else if (str[i] == 'o' || str[i] == 'O')
+{
+str[i] = '0';
+}
+else if (str[i] == 't' || str[i] == 'T')
+{
+str[i] = '7';
+}
+else if (str[i] == 'l' || str[i] == 'L')
+{
+str[i] = '1';
+}
+else if (str[i] == 'e' || str[i] == 'E')
+{
+str[i] = '3';
+}
+}
+return (str);
+}
