89250b907a161c96d22e385f112dc6f678273127
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-02 00:14:57 -0800

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,12 +10,12 @@ char *cap_string(char *str)
 int i = 0;
 for (i = 0; str[i] != '\0'; i++)
 {
-if (isspace(str[i]) > 0 || str[i] == '.' || str[i] == ',' || str[i] == ';' || str[i] == '!' || str[i] == '?' || str[i] =='(' || str[i] == ')' || str[i] == '{' || str[i] == '}' || str[i] == '\"')
+if (isspace(str[i]) > 0 || str[i] == '.' || str[i] == ',' || str[i] == ';' || str[i] == '!' || str[i] == '?' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}' || str[i] == '\"')
 {
 if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 {
 str[i + 1] = str[i + 1] - 32;
-} 
+}
 }
 }
 return (str);
