f5fa84174af653d1fc5c42c80f52c2d5a8bce18d
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-02 23:39:48 -0800

    fix 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-* _strcmp - function code
-*@s1: string 
-*@s2: string 
-* Return:number
-*/
+ * _strcmp - check the code
+ *@s1: string
+ *@s2: string
+ *
+ * Return: always string.
+ */
 int _strcmp(char *s1, char *s2)
 {
 return (strcmp(s1, s2));
