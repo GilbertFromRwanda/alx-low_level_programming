8b940e2e9586e7865ecdfce58620de8c159b6d44
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-06 01:16:17 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _memset - function code
+*@s: string
+*@b: string
+*@n: numeric
+* Return: string
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+return (memset(s, b, n));
+}
