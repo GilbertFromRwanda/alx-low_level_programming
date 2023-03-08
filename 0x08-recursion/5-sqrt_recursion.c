7930543400eee2887bfc8c82b7d188fed9ef774b
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-08 03:26:34 -0800

    implement my customer square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,15 +2,38 @@
 
 /**
  * _sqrt_recursion - check the code
- *@n: string
+ *@n:int
+ *
+ * mysqrt - fun to get square root
+ *@x: float
+ *@n: float
+ *@eps: constant last floot
  *
  * Return: Always sizeof s.
  */
+
+float mysqrt(float n, float x, float eps)
+{
+float y = (x + n / x) / 2;
+if (x - y < eps && y - x < eps)
+{
+return (y);
+}
+else
+{
+return (mysqrt(n, y, eps));
+}
+}
+
 int _sqrt_recursion(int n)
 {
-if (n < 0)
+float x = (float)n, eps = 0.000001, res;
+int trunc = 0;
+if( n < 0)
 {
 return (-1);
 }
-return (sqrt(n));
+res = mysqrt((int)n, x, eps);
+trunc =(int)res; 
+return (res == trunc ? trunc : -1);
 }
