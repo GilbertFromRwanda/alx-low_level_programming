a98d22716d7c37440be8aa9d8f608a3b41f652b7
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-08 02:20:00 -0800

    implement square root in c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - check the code
+ *@n: string
+ *
+ * Return: Always sizeof s.
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+return (sqrt(n));
+}
