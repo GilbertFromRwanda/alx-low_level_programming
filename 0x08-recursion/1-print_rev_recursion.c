d49b48130612c63d3fa05621820afa32869387f5
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-08 01:33:17 -0800

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,17 +3,20 @@
 /**
  * _print_rev_recursion - check the code
  *@s: string
- *
+ * reverse - code for reverse
+ *@str: string in reverse
+ *@index: index
+ *@n: integer with length of string
  * Return: Always 0.
  */
 
 void reverse(char *str, int index, int n)
 {
-if(index == n)
+if (index == n)
 {
 return;
 }
-reverse(str, index+1, n);
+reverse(str, index + 1, n);
 _putchar(str[index]);
 }
 void _print_rev_recursion(char *s)
