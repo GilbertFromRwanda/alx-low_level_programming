a1609971d83913cd96fa16ccb06402760e54bd80
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-08 03:51:40 -0800

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,17 +3,11 @@
 /**
  * is_prime_number - check the code
  *@n:int
- *
+ * is_prime - fun code prime number
+ *@i: int i
  * Return: Always sizeof s.
  */
-/**
- * is_prime - fun code checking prime number
- *@n: int
- *@i : in start at 2
- *
- * Return: 0 or 1
- *
-*/
+
 int is_prime(int n, int i)
 {
 if (n <= 2)
@@ -22,12 +16,12 @@ return (n == 2) ? 1 : 0;
 }
 if (n % i == 0)
 {
-return 0;
+return (0);
 }
 if (i * i > n) {
-return 1;
+return (1);
 }
-return is_prime(n, i + 1);
+return (is_prime(n, i + 1));
 }
 int is_prime_number(int n)
 {
