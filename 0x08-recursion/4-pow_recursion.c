1b9f879c7111a3c10f2821dcc9cceda33e9195fc
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-08 02:01:58 -0800

    pow in c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<math.h>
+
+/**
+ * _pow_recursion - check the code
+ *@x: integer
+ *@y: integer
+ * Return: Always sizeof s.
+ */
+int _pow_recursion(int x, int y)
+{
+return (pow(x, y));
+}
