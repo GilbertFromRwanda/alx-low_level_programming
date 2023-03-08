44049dddb92a320131fece21c1ce3c72311785fa
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-08 02:15:53 -0800

    implement pow in c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,11 +6,16 @@
  *@y: integer
  * Return: Always sizeof s.
  */
+
 int _pow_recursion(int x, int y)
 {
 if (y < 0)
 {
 return (-1);
 }
-return (pow(x, y));
+if (y == 0)
+{
+return (1);
+}
+ return (x * _pow_recursion(x, y - 1));
 }
