a5c844b441f13e5e5958b8679f9a22869dfd0777
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-10 03:11:49 -0800

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - function code
+*@argc: count size of array
+*@argv: array og argument known as vector
+* Return: always 0 or no zero
+*/
+int main(int argc, char *argv[])
+{
+if (argc)
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+extern void _putchar(char c);
+int  putchar(int  c);
+#endif
