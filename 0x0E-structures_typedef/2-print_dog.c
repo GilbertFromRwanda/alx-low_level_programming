c338d5c9383a847b553a03d487ee7e6faa577c42
GilbertFromRwanda <askforgilbert@gmail.com>
2023-03-21 07:13:45 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,9 +4,7 @@
  *print_dog - print
  *struct dog - dog struct
  *@d: for dog oject
- *
  */
-
 void print_dog(struct dog *d)
 {
 if(d == NULL)
@@ -15,15 +13,29 @@ return ;
 }
 else
 {
-if(d->name == NULL)
-{}
-if(d->owner == NULL)
+if(d.name == NULL)
 {
-printf("")
+printf("Name:(nil)\n");
 }
-if(d->age == NULL)
+else
 {
-
+printf("Name:%s\n", d.name);
+}
+if(d.owner == NULL)
+{
+printf("Owner:(nil)\n");
+}
+else
+{
+printf("Owner: %s\n", d.owner);
+}
+if(d.age == NULL)
+{
+printf("Age:(nil)\n");
+}
+else
+{
+printf("Age: %ld\n", d.age);
 }
 }
 }
